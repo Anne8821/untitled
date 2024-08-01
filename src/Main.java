65c84110ea2e6157716d7d2c42e38dd6b4b85944
Anne8821 <makurinaa@list.ru>
2024-08-01 12:59:16 +0300

    homework

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Привет, Мир!");
         var a = 1;
-        if (a >= 0) {
-            System.out.println("Число положительное")
+        if (a <= 0) {
+            System.out.println("Число отрицательное")
 
         }
     }
